034c9f635bd2f3bc17aa24ca4f0f07f220359ac1
Gian Lorenzo Meocci <glmeocci@gmail.com>
2018-07-07 14:01:00 +0200

    adding fill_source function

--- lib/adjust.ex
+++ lib/adjust.ex
@@ -2,28 +2,44 @@ defmodule Adjust do
   require Logger
 
   @doc """
-   Initialize the DBs
+   Initialize the DBs:
+     create two tables:
+     - "source" into :foo DB
+     - "dest" into :bar DB
   """
   def init() do
     with {:ok, conn} <- Adjust.Repo.connect(),
          {:ok, _result} <- Adjust.Repo.create_db(conn, "foo"),
          {:ok, _result} <- Adjust.Repo.create_db(conn, "bar") do
+      Logger.info("DBs created")
       create_tables()
     else
       _ -> Logger.error("is not possibile to initialize DB")
     end
   end
 
-  @doc """
-  create two tables:
-   - "source" into :foo DB
-   - "dest" into :bar DB
-  """
+  def fill_source(conn) do
+    with {:ok, conn} <- Adjust.Repo.connect("foo"),
+         {:ok, query} <- Adjust.Repo.get_source_query(conn) do
+      1..1_000_000
+      |> Enum.map(fn x ->
+        Adjust.Repo.insert_into_source(conn, query, [e, rem(e, 3), rem(e, 5)])
+      end)
+
+      ->(
+        _,
+        Logger.error("fill_source: is not possible to populate source")
+      )
+    end
+  end
+
+  ### PRIVATE ###
+
   defp create_tables() do
-    with {:ok, coon_foo} <- Adjust.Repo.connect("foo"),
-         {:ok, coon_bar} <- Adjust.Repo.connect("bar"),
-         :ok <- Adjust.Repo.create_table(coon_foo, "source"),
-         :ok <- Adjust.Repo.create_table(coon_bar, "dest") do
+    with {:ok, conn_foo} <- Adjust.Repo.connect("foo"),
+         {:ok, conn_bar} <- Adjust.Repo.connect("bar"),
+         {:ok, _r} <- Adjust.Repo.create_table(conn_foo, "source"),
+         {:ok, _r} <- Adjust.Repo.create_table(conn_bar, "dest") do
       Logger.info("table created")
     else
       _ -> Logger.error("is not possibile to create table")
--- lib/adjust/repo.ex
+++ lib/adjust/repo.ex
@@ -24,4 +24,12 @@ defmodule Adjust.Repo do
   def create_table(conn, table_name) do
     Postgrex.query(conn, "CREATE TABLE #{table_name} (a integer, b integer, c integer)", [])
   end
+
+  def get_source_query(conn) do
+    Postgrex.prepare(conn, "INSERT INTO source(a, b, c) VALUE($1, $2, $3)")
+  end
+
+  def insert_into_source(conn, query, params) do
+    Postgrex.execute(conn, query, params)
+  end
 end
