6038412b23d745bc9b40eecde78616192f393abf
Gian Lorenzo Meocci <glmeocci@gmail.com>
2018-07-07 11:17:41 +0200

    init project

---
+++ lib/adjust.ex
@@ -0,0 +1,18 @@
+defmodule Adjust do
+  @moduledoc """
+  Documentation for Adjust.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Adjust.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
